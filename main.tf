ae2d0c22cedf00468bf0780e2375ab8071de3ed9
diyadevi21 <malikdiya627@gmail.com>
2025-04-10 20:19:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "docker" {}
+
+resource "docker_image" "nginx_image" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = docker_image.nginx_image.name
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
